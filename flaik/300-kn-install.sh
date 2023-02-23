1a00489d5fb52139af074cbdef3df70291616d8a
ShixiongQi <qishixiong19931009@gmail.com>
2023-02-22 23:57:07 -0800

    Fix knative deployment yaml

--- flaik/300-kn-install.sh
+++ flaik/300-kn-install.sh
@@ -10,9 +10,9 @@ fi
 
 echo 'Install Knative Serving v0.22.0'
 kubectl apply -f https://github.com/knative/serving/releases/download/v0.22.0/serving-crds.yaml
-kubectl apply -f ./sigcomm-experiment/env-setup/serving-core.yaml
+kubectl apply -f ./flaik/serving-core.yaml
 
 echo 'Install Istio v0.22.0'
-kubectl apply -f ./sigcomm-experiment/env-setup/istio.yaml # Install a properly configured Istio
+kubectl apply -f ./flaik/istio.yaml # Install a properly configured Istio
 kubectl apply -f https://github.com/knative/net-istio/releases/download/v0.22.0/net-istio.yaml # Install the Knative Istio controller
 kubectl --namespace istio-system get service istio-ingressgateway # Fetch the External IP or CNAME
